bd941c742d4251113514077fe673081039d2b4fe
Haris Hussain <syedharishussain@gmail.com>
2012-11-20 23:47:27 +0500

    Initial Commit

---
+++ iOSBoilerPlate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSBoilerPlate
+//
+//  Created by Haris on 11/20/12.
+//  Copyright (c) 2012 Haris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
