3638efcb623ef9822ca30bf2e18e31d318822e1c
Haris Hussain <syedharishussain@gmail.com>
2012-11-20 23:57:33 +0500

    Moved Files in Respective Dirs

---
+++ iOSBoilerPlate/Controller/AppDelegate/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOSBoilerPlate
+//
+//  Created by Haris on 11/20/12.
+//  Copyright (c) 2012 Haris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOSBoilerPlate/Controller/ViewControllers/RootViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSBoilerPlate
+//
+//  Created by Haris on 11/20/12.
+//  Copyright (c) 2012 Haris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
